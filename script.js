69001cecaac428cbcfc6a9cfbb1e9d4efb7b379d
Shuliii <ricoharsono@gmail.com>
2022-11-08 21:06:20 +0800

    push fetch chart

--- script.js
+++ script.js
@@ -1,20 +1,81 @@
-// function setWeekDays() {
-//   fetch("data.json")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data);
-//     });
-// }
-// setWeekDays();
-let finalData;
+const chartContainer = document.querySelector(".chart-container");
 
-fetch("data.json")
+// let data = [
+//   {
+//     day: "mon",
+//     amount: 17.45,
+//   },
+//   {
+//     day: "tue",
+//     amount: 34.91,
+//   },
+//   {
+//     day: "wed",
+//     amount: 52.36,
+//   },
+//   {
+//     day: "thu",
+//     amount: 31.07,
+//   },
+//   {
+//     day: "fri",
+//     amount: 23.39,
+//   },
+//   {
+//     day: "sat",
+//     amount: 43.28,
+//   },
+//   {
+//     day: "sun",
+//     amount: 25.48,
+//   },
+// ];
+
+// let finalData;
+const highestAmount = Math.max(...data.map((el) => el.amount));
+
+const newData = fetch("data.json")
   .then((res) => res.json())
   .then((data) => {
-    finalData = data;
+    data.map((el) => {
+      if (el.amount === highestAmount) {
+        height = 150;
+        return `<div class="chart">
+            <div class="chart-amount hidden">${el.amount}</div>
+            <div class="block-high blue" style="height: ${height}px"></div>
+            <p>${el.day}</p>
+        </div>`;
+      } else {
+        height = (el.amount / highestAmount) * 150;
+        return `<div class="chart">
+        <div class="chart-amount hidden">${el.amount}</div>
+            <div class="block" style="height: ${height}px"></div>
+            <p>${el.day}</p>
+        </div>`;
+      }
+    });
   })
   .catch((err) => {
     console.log(err);
   });
 
-console.log(finalData);
+// const newData = data.map((el) => {
+//   if (el.amount === highestAmount) {
+//     height = 150;
+//     return `<div class="chart">
+//         <div class="chart-amount hidden">${el.amount}</div>
+//         <div class="block-high blue" style="height: ${height}px"></div>
+//         <p>${el.day}</p>
+//     </div>`;
+//   } else {
+//     height = (el.amount / highestAmount) * 150;
+//     return `<div class="chart">
+//     <div class="chart-amount hidden">${el.amount}</div>
+//         <div class="block" style="height: ${height}px"></div>
+//         <p>${el.day}</p>
+//     </div>`;
+//   }
+// });
+
+const insertHTML = newData.join("");
+chartContainer.innerHTML = insertHTML;
