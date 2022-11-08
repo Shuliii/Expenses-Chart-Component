9f359f97a45549350bbf5416e12a611a42173cdd
Shuliii <ricoharsono@gmail.com>
2022-11-08 19:45:59 +0800

    edit multiple files

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function setWeekDays() {
-  fetch("data.json")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-}
-setWeekDays();
+// function setWeekDays() {
+//   fetch("data.json")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// }
+// setWeekDays();
+let finalData;
+
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    finalData = data;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+console.log(finalData);
