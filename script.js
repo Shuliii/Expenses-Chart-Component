a0dd03ddadfcccaf947a72c1ae52d16cbe72205b
Shuliii <ricoharsono@gmail.com>
2022-11-09 15:02:14 +0800

    edit

--- script.js
+++ script.js
@@ -38,7 +38,25 @@ fetch("data.json")
   .then((res) => res.json())
   .then((data) => {
     let max = Math.max(...data.map((el) => el.amount));
-    console.log(max);
+    let newData = data.map((el) => {
+      console.log(el);
+      if (el.amount === max) {
+        height = 150;
+        return `<div class="chart">
+                <div class="chart-amount hidden">${el.amount}</div>
+                <div class="block-high blue" style="height: ${height}px"></div>
+                <p>${el.day}</p>
+            </div>`;
+      } else {
+        height = (el.amount / max) * 150;
+        return `<div class="chart">
+            <div class="chart-amount hidden">${el.amount}</div>
+                <div class="block" style="height: ${height}px"></div>
+                <p>${el.day}</p>
+            </div>`;
+      }
+      console.log(newData);
+    });
   })
   .catch((err) => {
     console.log(err);
